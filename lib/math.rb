5d297daac2d8a84c44fdceb1325d81bdff2cf20e
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-12 20:08:08 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,15 +3,15 @@ sum=num1+num2
 end
 
 def subtraction(num1, num2)
-
+difference=num1+num2
 end
 
 def division(num1, num2)
-
+=num1+num2
 end
 
 def multiplication(num1, num2)
-
+product=num1+num2
 end
 
 def modulo(num1, num2)
