df27b5588c9a45e70f40e467dce235c58207d055
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-12 20:23:19 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
+num1=5
+num2=4
+sum=num1+num2
 
 end
 
 def subtraction(num1, num2)
-
+num1=10
+num2=5
+difference=num1-num2
 end
 
 def division(num1, num2)
-
+num1=50
+num2=2
+quotient=num1/num2
 end
 
 def multiplication(num1, num2)
-
+num1=4
+num2=30
+product=num1*num2
 end
 
 def modulo(num1, num2)
-
+num1=34
+num2=5
+remainder=num1%num2
 end
 
 def square_root(num)
-
+num=81
+Math.sqrt(num)
 end
