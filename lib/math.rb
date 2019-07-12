2cd489bc0b5a5f675d0813ab7c5177aecf95863b
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-12 20:09:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,11 +3,11 @@ sum=num1+num2
 end
 
 def subtraction(num1, num2)
-difference=num1+num2
+difference=num1-num2
 end
 
 def division(num1, num2)
-=num1+num2
+quotient=num1/num2
 end
 
 def multiplication(num1, num2)
@@ -15,9 +15,9 @@ product=num1+num2
 end
 
 def modulo(num1, num2)
-
+remainder=num1+num2
 end
 
 def square_root(num)
-
+square_root=num1+num2
 end
